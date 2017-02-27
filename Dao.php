c0c624bbaf038ef1a2f84cd5215bff52c475cdf3
joshua-holden <joshuaholden@u.boisestate.edu>
2017-02-27 15:45:58 -0700

    Added sql file

--- Dao.php
+++ Dao.php
@@ -9,15 +9,4 @@ class Dao {
       new PDO("mysql:host={$this->host};dbname={$this->db}", $this->user,
           $this->pass);
   }
-  public function getComments () {
-    $conn = $this->getConnection();
-    return $conn->query("select comment, date_entered from comment order by date_entered desc");
-  }
-  public function saveComment ($comment) {
-    $conn = $this->getConnection();
-    $saveQuery = "insert into comment (comment, user_id) values (:comment, 1)";
-    $q = $conn->prepare($saveQuery);
-    $q->bindParam(":comment", $comment);
-    $q->execute();
-  }
 }
